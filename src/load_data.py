43a3509260ece00227a601142244ccd46490a239
James Heald <jh4069@columbia.edu>
2023-01-04 14:28:17 +0000

    store images, fraction of data

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import jax.numpy as np
+import numpy as onp
 # hide any GPUs from TensorFlow, otherwise TF might reserve memory and make it unavailable to JAX
 tf.config.set_visible_devices([], device_type = 'GPU')
 
@@ -17,24 +19,42 @@ def prepare_image(dictionary):
 
     return dictionary
 
-def transform_dataset(dataset, batch_size, tfds_seed):
+def transform_train_dataset(dataset, batch_size, tfds_seed):
     
     dataset = dataset.cache()
     dataset = dataset.shuffle(tf.data.experimental.cardinality(dataset).numpy(), seed = tfds_seed, reshuffle_each_iteration = True)
-    dataset = dataset.batch(batch_size, drop_remainder = True).prefetch(1)
+    dataset = dataset.batch(batch_size, drop_remainder = True)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    
+    return dataset
+
+def transform_validate_dataset(dataset):
+    
+    dataset = dataset.cache()
+    dataset = dataset.batch(tf.data.experimental.cardinality(dataset).numpy())
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    dataset = np.array(list(dataset)[0]['image'])
     
     return dataset
 
 def create_data_split(cfg):
 
-    (full_train_set, test_dataset), ds_info = \
-    tfds.load('Omniglot', split = ['train', 'test'], shuffle_files = True, as_supervised = False, with_info = True)
+    train_str = 'train[:{}%]'.format(cfg.percent_data_to_use)
+    test_str = 'test[:{}%]'.format(cfg.percent_data_to_use)
 
+    (full_train_set, test_dataset), ds_info = \
+    tfds.load('Omniglot', split = [train_str, test_str], shuffle_files = True, as_supervised = False, with_info = True)
+    
     full_train_set = full_train_set.map(prepare_image, num_parallel_calls = tf.data.AUTOTUNE)
 
-    num_data = tf.data.experimental.cardinality(full_train_set).numpy()
-    train_dataset = full_train_set.take(num_data * (1 - cfg.validation_split))
-    validate_dataset = full_train_set.take(num_data * (cfg.validation_split))
-    train_dataset = transform_dataset(train_dataset, cfg.batch_size, cfg.tfds_seed)
-
+    n_data = tf.data.experimental.cardinality(full_train_set).numpy()
+    n_data_validate = tf.cast(n_data * (cfg.fraction_for_validation), tf.int64)
+    n_data_train = tf.cast(n_data * (1 - cfg.fraction_for_validation), tf.int64)
+    
+    train_dataset = full_train_set.take(n_data_train)
+    validate_dataset = full_train_set.skip(n_data_train).take(n_data_validate)
+    
+    train_dataset = transform_train_dataset(train_dataset, cfg.batch_size, cfg.tfds_seed)
+    validate_dataset = transform_validate_dataset(validate_dataset)
+    
     return train_dataset, validate_dataset, test_dataset, ds_info
